5e598aaf8e455e88330ee7e91addc6393a356ec6
farfun <1007530194@qq.com>
2025-05-16 18:13:01 +0800

    chore: 更新项目版本并重构导入语句
    - 将pyproject.toml中的版本从1.0.11更新至1.0.12
    - 重构__init__.py中的导入语句，统一从command模块导入
    
    这些更改属于维护性更新，不影响功能行为。

--- src/funinstall/install/__init__.py
+++ src/funinstall/install/__init__.py
@@ -1,8 +1,5 @@
 from .command import app as install_app
-from .coder_server import install_code_server
-from .go import install_go
-from .new_api import install_newapi
-from .nodejs import install_nodejs
+from .command import install_code_server, install_go, install_newapi, install_nodejs
 
 __all__ = [
     "install_app",
