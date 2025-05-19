ba0e6b36db2b9e582c6f2cfebee9b59fdde3ee76
Liangtao Niu <bingtao@bingtao-dev.local>
2025-05-19 16:28:23 +0800

    add

--- src/funinstall/install/base.py
+++ src/funinstall/install/base.py
@@ -24,6 +24,7 @@ class BaseInstall:
             logger.error("无法识别当前系统")
             return False
 
+
     @abstractmethod
     def install_linux(self, *args, **kwargs) -> bool:
         pass
@@ -32,4 +33,27 @@ class BaseInstall:
         return self.install_linux(*args, **kwargs)
 
     def install_windows(self, *args, **kwargs) -> bool:
+        return self.install_windows(*args, **kwargs)
+
+    def uninstall(self, *args, **kwargs) -> bool:
+        if sys.platform.startswith("linux"):
+            logger.info("当前系统为 Linux")
+            return self.uninstall_linux(*args, **kwargs)
+        elif sys.platform.startswith("darwin"):
+            logger.info("当前系统为 macOS")
+            return self.uninstall_macos(*args, **kwargs)
+        elif sys.platform.startswith("win"):
+            logger.info("当前系统为 Windows")
+            return self.uninstall_windows(*args, **kwargs)
+        else:
+            logger.error("无法识别当前系统")
+            return False
+
+    def uninstall_linux(self, *args, **kwargs) -> bool:
+        pass
+
+    def uninstall_macos(self, *args, **kwargs) -> bool:
+        return self.install_linux(*args, **kwargs)
+
+    def uninstall_windows(self, *args, **kwargs) -> bool:
         return self.install_windows(*args, **kwargs)
